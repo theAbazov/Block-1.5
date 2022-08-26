1e4d562f29a20e2316f6fde5a0fa5fdbd974c141
Isa <abazovkjks@gmail.com>
2022-08-26 14:30:04 +0600

    bags-1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let swiper = new Swiper(window.innerWidth < 768 ? ".mySwiper" : '', {
   slidesPerView: "auto",
-  spaceBetween: 0,
+  spaceBetween: 16,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
